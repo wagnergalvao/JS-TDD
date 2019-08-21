ebea0ec99462c0a47dd9b7ab7c18ae5ab48d537d
Wagner José Bitencourt Galvão <galvao.wagner@outlook.com.br>
2019-08-21 15:40:17 -0300

    Fixing main.js

--- src/main.js
+++
@@ -1,6 +0,0 @@
-const a = 3;
-const b = 2;
-const d = 4;
-const c = 'jwencwjencjiwencw ijecn wjioc nwejioc bewcibwiecbwijcwejecbweijcbwijvbwejcecjwecpiwebcpiwebcpiwiebcpiwebcpiwebcipwebcpiwebcwpieibcpwiec';
-console.log(a + b + d);
-console.log(c);
