00f701331102ddfbe585c438d4d10de28ca2c577
Wagner José Bitencourt Galvão <galvao.wagner@outlook.com.br>
2019-08-21 10:03:01 -0300

    Create editorconfig

---
+++ src/main.js
@@ -0,0 +1,6 @@
+const a = 3;
+const b = 2;
+const d = 4;
+const c = 'jwencwjencjiwencw ijecn wjioc nwejioc bewcibwiecbwijcwejecbweijcbwijvbwejcecjwecpiwebcpiwebcpiwiebcpiwebcpiwebcipwebcpiwebcwpieibcpwiec';
+console.log(a + b + d);
+console.log(c);
