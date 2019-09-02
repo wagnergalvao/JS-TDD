4213ba88e23af1a0ba156af1d474e0f70387fe76
Wagner José Bitencourt Galvão <galvao.wagner@outlook.com.br>
2019-09-02 17:56:17 -0300

    Add Webpack & Babel

--- src/index.js
+++ src/index.js
@@ -3,6 +3,6 @@ import * as R from 'ramda';
 const arr1 = [1, 1, 1, 2, 2, 3, 4, 5, 6, 7, 8];
 const arr2 = [5, 5, 5, 6, 6, 6, 7, 8, 8, 9, 9, 1];
 
-const arr3 = R.union(arr1,arr2);
+const arr3 = R.union(arr1, arr2);
 
 console.log(arr3);
