c8a6a2248370231114964d8b342919fd1ba2740c
Wagner José Bitencourt Galvão <galvao.wagner@outlook.com.br>
2019-09-09 12:00:13 -0300

    Change index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,29 @@
-import * as R from 'ramda';
+/**
+ * import
+ */
+ import * as R from 'ramda';
 
+/**
+ * [arr1 Sequência de valores]
+ * @type {Array}
+ */
 const arr1 = [1, 1, 1, 2, 2, 3, 4, 5, 6, 7, 8];
+/**
+ * [arr2 Sequência de valores]
+ * @type {Array}
+ */
 const arr2 = [5, 5, 5, 6, 6, 6, 7, 8, 8, 9, 9, 1];
-
+/**
+ * [arr3 array com elementos únicos extraídos da união de arr1 e arr2]
+ * @type {[type]}
+ */
 const arr3 = R.union(arr1, arr2);
+/**
+ * [arr4 array com elementos únicos extraídos de arr1]
+ * @type {[type]}
+ */
+const arr4 = R.uniq(arr1);
 
 console.log(arr3);
+
+console.log(arr4);
